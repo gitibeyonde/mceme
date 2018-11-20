ad13823d25551f6db121431aff14cea84681ef26
TECHSID <siddharth@ibeyonde.com>
2018-11-20 16:04:52 +0530

    creating grid of spectogram to visualise

--- train/train/preprocess.py
+++ train/train/preprocess.py
@@ -1,4 +1,5 @@
 import glob
+import uuid
 from scipy.io import wavfile
 from scipy.fftpack import fft
 import matplotlib
@@ -7,11 +8,39 @@ import matplotlib.pyplot as plt
 from PIL import Image
 import os,cv2
 import numpy as np
+import fnmatch
+from skimage.io import imread
 #read wave file
 def readwave(file):
     sr,data = wavfile.read(file)
     return sr,data
 
+def plot_images(folder):
+    uuidu = str(uuid.uuid4().hex)
+    os.chdir('../spectrumanalysis')
+    os.mkdir(uuidu)
+    saving_folder = uuidu+'/'
+    i = 0
+    plt.subplots_adjust(hspace=0.1)
+    for root, dirnames, filenames in os.walk(folder):
+        j = 0
+        for filename in fnmatch.filter(filenames, '*.jpg'):
+            if (i==8):
+                j = j+1
+                plt.savefig(saving_folder+root.split('/')[-1]+str(j)+'.png',
+                    dpi=900)
+                i=0
+                plt.close()
+            ax = plt.subplot(2,4,i+1)
+            ax.set_title(filename,size=3)
+            ax.axes.xaxis.set_ticklabels([])
+            ax.axes.yaxis.set_ticklabels([])
+            im = imread(os.path.join(root, filename))
+            i+=1
+            imgplot = ax.imshow(im)
+    return
+    
+    
 def imagepreprocess(image,height,width):
     i = 0 
     current = cv2.imread(image)
@@ -51,6 +80,20 @@ def npfourierfft(wav_file):
     print (fft_out.shape)  
       
 
+def getspecdata(wav_file):
+
+    sr,data = readwave(wav_file)
+    if (len(data)==0):
+        return 0
+    print ("reading file "+wav_file+" with size %d"%(len(data)))
+    nfft = 256
+    sf = 256
+    pxx, freqs, bins, im = plt.specgram(data, nfft, sf)
+    print pxx.shape
+    return pxx
+
+
+
 def graphspectogram(wav_file):
     sr,data = readwave(wav_file)
     if (len(data)==0):
@@ -62,6 +105,11 @@ def graphspectogram(wav_file):
 #    print ("pxx : ", len(pxx))
  #   print ("freqs : ", len(freqs))
   #  print ("bins : ", len(bins))
+    print (freqs)
+    print (pxx.shape)
+    print (freqs.shape)
+
+    #plt.show()
     plt.savefig(wav_file.split('.wav')[0] + '.png',
                 dpi=100,  # Dots per inch
                 frameon='false',
@@ -79,7 +127,8 @@ def graphspectogram(wav_file):
         #os.system('convert '+(wav_file.split('.wav')[0] + '.png') + ' '+(wav_file.split('.wav')[0] + '.jpg'))
         os.remove(wav_file.split('.wav')[0] + '.png')
     return 1
-#iterate over all the folders and create the spectogram            
+#iterate over all the folders and create the spectogram     
+
 def wav_to_spec(path):
     files = glob.glob(path + "/*.wav")
     for f in files:
@@ -89,8 +138,22 @@ def wav_to_spec(path):
             return val
         except Exception as e:
             print ("Something went wrong while generating spectrogram:", e) 
+
             
- 
+def savespecdata(path):
+    data = list()
+    for root, dirnames, filenames in os.walk(path):
+        for filename in fnmatch.filter(filenames, '*.wav'):
+            try:
+                print (os.path.join(root, filename))
+                data.append(getspecdata(os.path.join(root, filename)))
+            except Exception as e:
+                print ("Something went wrong while generating spectrogram:", e) 
+    print(data[0])            
+    new = np.array(data)
+    np.savetxt('train.txt',new,fmt ='%d')            
+    
+
 def fourier(path):
     files = glob.glob(path + "/*.wav")
     for f in files:
---
+++ train/wavspec1.py
@@ -0,0 +1,20 @@
+'''
+Created on 01-Nov-2018
+
+@author: techsid
+'''
+
+from train import preprocess
+ 
+'''  
+preprocess.wav_to_spec('/Users/techsid/Desktop/eded/dur_1/eded/crawling_man/crawling_man_75m/')
+preprocess.wav_to_spec('/Users/techsid/Desktop/eded/dur_1/eded/crawling_man/crawling_man_100m/')
+preprocess.wav_to_spec('/Users/techsid/Desktop/eded/dur_1/eded/crawling_man/crawling_man_125m/')
+preprocess.wav_to_spec('/Users/techsid/Desktop/eded/dur_1/eded/crawling_man/crawling_man_150m/')
+preprocess.wav_to_spec('/Users/techsid/Desktop/eded/dur_1/eded/crawling_man/crawling_man_175m/')
+preprocess.wav_to_spec('/Users/techsid/Desktop/eded/dur_1/eded/gp_of_man/gp_of_men_100m/')
+preprocess.wav_to_spec('/Users/techsid/Desktop/eded/dur_1/eded/gp_of_man/gp_of_men_150m/')
+preprocess.wav_to_spec('/Users/techsid/Desktop/eded/dur_1/eded/gp_of_man/gp_of_men_200m/')
+'''
+
+preprocess.plot_images('/Users/techsid/Desktop/eded/dur_1/eded')
