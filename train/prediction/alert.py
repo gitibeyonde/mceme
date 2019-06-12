f8b55170a9988b2841690525c7c4c0e4454061c9
Abhinandan Prateek <abhinandan.prateek@shapeblue.com>
2019-06-12 11:17:59 +0530

    adding tank_life sources

---
+++ train/prediction/alert.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import subprocess
+
+subprocess.Popen(["/Users/abhi/work/mceme/ai/train/prediction/men_alert.py"], shell=False,stdin=None,stdout=None,stderr=None,close_fds=True)
+                       
+subprocess.Popen(["/Users/abhi/work/mceme/ai/train/prediction/vehicle_alert.py"], shell=False,stdin=None,stdout=None,stderr=None,close_fds=True)
+
+subprocess.Popen(["/Users/abhi/work/mceme/ai/train/prediction/men_alert.py"], shell=False,stdin=None,stdout=None,stderr=None,close_fds=True)
---
+++ train/prediction/vehicle_alert.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import cv2
+import sys
+import time
+
+
+if __name__ == "__main__":
+    img = cv2.imread('/Users/abhi/work/mceme/ai/train/prediction/vehicle.jpg')
+    cv2.imshow('image', img)
+    cv2.waitKey(10000)
+    cv2.destroyAllWindows()
+    sys.exit(0)
+
